4cea614413130fb501a59c80c17b7007b19410be
Kushagra <96912080+candy-kush@users.noreply.github.com>
2023-07-22 20:30:04 +0530

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -100,8 +100,8 @@ var swiper = new Swiper('.slide-content', {
 function sendEmail() {
     Email.send({
         Host: 'smtp.gmail.com',
-        Username: 'kushagraranjan987@gmail.com',
-        Password: 'Ku$h@gra@gm@il987.',
+        Username: 'kushagraranjangpj@gmail.com',
+        Password: '',
         To: 'kushagraranjangpj@gmail.com',
         From: document.getElementById('email').value,   
         Subject: document.getElementById('emailSub').value,
@@ -120,8 +120,8 @@ var icon = document.getElementById('dark-icon');
 icon.onclick = function() {
     document.body.classList.toggle('dark-theme');
     if(document.body.classList.contains('dark-theme')) {
-        icon.src = "/../images/sun-solid-24.png";
+        icon.src = "../images/sun-solid-24.png";
     } else {
-        icon.src = "/../images/moon-solid-24.png";
+        icon.src = "../images/moon-solid-24.png";
     }
 }
